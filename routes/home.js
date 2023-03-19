fc6d77943230a2a4308967595870b2a244c70afb
HÀO HOÀNG <haohoangofficial@gmail.com>
2023-03-19 21:50:12 +0700

    update

--- routes/home.js
+++ routes/home.js
@@ -7,5 +7,11 @@ router.get("/", async (req, res, next) => {
     message: "The app is working properly!",
   });
 });
+router.get("/webhook", async (req, res, next) => {
+  return res.status(200).json({
+    title: "Webhook",
+    message: "The app is Webhook!",
+  });
+});
 
 module.exports = router;
